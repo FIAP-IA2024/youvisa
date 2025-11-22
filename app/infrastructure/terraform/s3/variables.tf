bb25e5255c8ab6f4745618b4200844c04031f83a
ribeirogab <gblosr@gmail.com>
2025-11-22 20:36:58 -0300

    feat: Set default value for S3 bucket name in Terraform variables

--- app/infrastructure/terraform/s3/variables.tf
+++ app/infrastructure/terraform/s3/variables.tf
@@ -13,6 +13,7 @@ variable "environment" {
 variable "s3_bucket_name" {
   description = "Name of the S3 bucket for storing files"
   type        = string
+  default     = "youvisa-files-9k3m7x"
 }
 
 variable "aws_region" {
