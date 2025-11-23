6d05aafab085c17261f0fd2405dd17973c8f0e70
ribeirogab <gblosr@gmail.com>
2025-11-23 18:38:24 -0300

    feat: simplify S3 event notification configuration by removing unnecessary filter_prefix

--- app/infrastructure/terraform/ocr/s3-events.tf
+++ app/infrastructure/terraform/ocr/s3-events.tf
@@ -3,9 +3,8 @@ resource "aws_s3_bucket_notification" "file_upload_notification" {
   bucket = data.aws_s3_bucket.files.id
 
   queue {
-    queue_arn     = aws_sqs_queue.ocr_queue.arn
-    events        = ["s3:ObjectCreated:*"]
-    filter_prefix = "telegram/"
+    queue_arn = aws_sqs_queue.ocr_queue.arn
+    events    = ["s3:ObjectCreated:*"]
   }
 
   depends_on = [aws_sqs_queue_policy.ocr_queue_policy]
